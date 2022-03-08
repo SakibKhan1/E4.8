9a4821da7e96a2523eb9c838926089de421b0198
Sakib Khan <37256061+SakibKhan1@users.noreply.github.com>
2022-03-07 20:14:22 -0500

    Create HomeworkE4.8.cpp

---
+++ HomeworkE4.8.cpp
@@ -0,0 +1,16 @@
+//Name: Sakib Khan
+//CSCI 135
+//Homework E4.8
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    string word;
+    cout << "Enter a word: ";
+    cin >> word;
+    for (int i = 0; i < word.length(); i++)
+        cout << word[i] << endl;
+    return 0;
+}
+
